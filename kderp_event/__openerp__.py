ec03f00f5387285881364f6a7cd60df67f2a58a5
dautienthuy <dautienthuy@gmail.com>
2015-09-21 13:33:46 +0700

    [ADD]Them module moi ke thua module website_events

---
+++ kderp_event/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Kderp Events",
+    'summary': """Kderp Events Extend Events Module""",
+    'description': """
+            Kderp Events Extend Events Module
+            """,
+    'author': "Team IT Kinden",
+    'website': "http://www.kinden.com.vn",
+    'category': 'Demo Events',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['website_event'],
+    # always loaded
+    'data': [ 
+             'templates.xml',
+    ],
+    'demo':[
+           ]
+}
