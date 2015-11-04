dc850bce2a7daaea80de000659086b4a955ed0b3
THCN <thanhacun@gmail.com>
2015-11-04 22:33:48 +0700

    [REF] optimizing template; DRY  code

--- kderp_website_theme_default/static/src/js/theme.js
+++ kderp_website_theme_default/static/src/js/theme.js
@@ -5,11 +5,11 @@
     if (window.location.pathname !== "/") {
       if ($("#list-features").text().trim()) {
         //Showing features
-        $("#list-main").addClass("col-md-8")
+        //$("#list-main").addClass("col-md-8")
         $("#list-features").addClass("col-md-4");
       } else {
         //Center list main
-        $("#list-main").addClass("col-md-8 col-md-push-2");
+        $("#list-main").addClass("col-md-push-2");
         $("#list-pager").addClass("col-md-push-2");
       }
     }
