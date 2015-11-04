72508e573dd0c0772dde5a0713fac67b37fb5bad
THCN <thanhacun@gmail.com>
2015-11-04 18:12:46 +0700

    [REF] minimizing templates number

--- kderp_website_theme_default/static/src/js/theme.js
+++ kderp_website_theme_default/static/src/js/theme.js
@@ -2,13 +2,16 @@
   'use strict';
   $(document).ready(function() {
     //console.log($("#list-features").text().trim() === "");
-    if ($("#list-features").text().trim()) {
-      //Showing features
-      $("#list-features").addClass("col-md-4");
-    } else {
-      //Center list main
-      $("#list-main").addClass("col-md-push-2");
-      $("#list-pager").addClass("col-md-push-2");
+    if (window.location.pathname !== "/") {
+      if ($("#list-features").text().trim()) {
+        //Showing features
+        $("#list-main").addClass("col-md-8")
+        $("#list-features").addClass("col-md-4");
+      } else {
+        //Center list main
+        $("#list-main").addClass("col-md-8 col-md-push-2");
+        $("#list-pager").addClass("col-md-push-2");
+      }
     }
   });
 })();
