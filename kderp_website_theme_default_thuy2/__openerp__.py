a0ce158a9e91a88b49a726371d789bf59e329752
Administrator <dutienthuy@gmail.com>
2016-06-16 08:55:29 +0700

    [ADD]Them theme moi

---
+++ kderp_website_theme_default_thuy2/__openerp__.py
@@ -0,0 +1,23 @@
+{
+    #Theme infomation
+    'name':'Kderp Website Theme Default Thuy2',
+    'description':"""
+        Kderp Website Theme Default Thuy2
+    """,
+    'category':'test',
+    'version':'1.0',
+    'depends':['kderp_website', 'kderp_website_theme_default', 'website_menu_by_user_status'],
+    
+    #tempalte, pages, and snippets
+    'data':[
+            'views/layout.xml',
+            'views/pages.xml',
+            'views/data.xml',
+            'views/assets.xml',
+            ],
+    
+    #Information
+    'author': "IT_Kinden",
+    'website':"www.kinden.com.vn",
+    'application':True,
+}
