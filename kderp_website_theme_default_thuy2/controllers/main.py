4707e5130353d03d99db56149e2d578f27d6a8aa
dautienthuy <dautienthuy@gmail.com>
2017-04-12 04:10:37 +0000

    [IMP]Controller(main), Views(data, pages, snippets): Thay OnGoing thanh Featured

--- kderp_website_theme_default_thuy2/controllers/main.py
+++ kderp_website_theme_default_thuy2/controllers/main.py
@@ -7,14 +7,14 @@ from openerp.tools.translate import _
 from openerp.http import request
 
 #Ke thua class KderpWebsite trong module kderp_website
-#Tao trang Project Ongoing va Project Completed
+#Tao trang Project Featured va Project Completed
 from openerp.addons.kderp_website.controllers.main import KderpWebsite
 
 class ExtendKderpWebsite(KderpWebsite):
 	post_limit = 9
-	@http.route(['/pj/ongoing','/pj/ongoing/page/<int:page>'],  auth='public', website=True)
-	def kdvn_pj_ongoing(self, page=1 , pager_url="/pj/ongoing"):
-		posts = http.request.env['blog.post'].search([('blog_id', '=', 'Project Ongoing')])
+	@http.route(['/pj/featured','/pj/featured/page/<int:page>'],  auth='public', website=True)
+	def kdvn_pj_featured(self, page=1 , pager_url="/pj/featured"):
+		posts = http.request.env['blog.post'].search([('blog_id', '=', 'Project Featured')])
 		list_posts = posts[(page - 1) * self.post_limit:page * self.post_limit]
 		pager = request.website.pager(
 			url=pager_url,
@@ -22,7 +22,7 @@ class ExtendKderpWebsite(KderpWebsite):
 			total=len(posts),
 			step=self.post_limit
 			)
-		return http.request.render('kderp_website_theme_default_thuy2.page_pj_ongoing', {'posts': list_posts, 'pager': pager,})
+		return http.request.render('kderp_website_theme_default_thuy2.page_pj_featured', {'posts': list_posts, 'pager': pager,})
 		
 	@http.route(['/pj/completed','/pj/completed/page/<int:page>'], auth='public', website=True)
 	def kdvn_pj_completed(self, page=1 , pager_url="/pj/completed"):
