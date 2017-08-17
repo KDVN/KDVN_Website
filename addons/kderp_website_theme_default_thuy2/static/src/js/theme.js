8c3c8f0b414e1f80382b921a4b825cdeba6a87a9
dautienthuy <dautienthuy@gmail.com>
2017-08-17 09:56:06 +0000

    [IMP]Static(css, js), Views(assets,pages): Them 2 Template de Demo trong Project Featured, Template 1 de hien thi noi dung khong co anh, Template 2 de hien thi Slice

--- addons/kderp_website_theme_default_thuy2/static/src/js/theme.js
+++ addons/kderp_website_theme_default_thuy2/static/src/js/theme.js
@@ -1,5 +1,27 @@
+//An hien menu trai trong cac trang Project
 if( $("#xs-check").is(":visible") )
     $("#kdvn-collapse").removeClass("in");
     
 if( $("#xs-check").is(":visible") )
     $("#kdvn-collapse-comp").removeClass("in");
+
+//Cho slice trong trang Project
+jQuery(function() {
+	jQuery('#allinone_carousel_charming').allinone_carousel({
+		skin: 'charming',
+		width: 850,
+		height: 454,
+		responsive:true,
+		autoPlay: 3,
+		resizeImages:true,
+		autoHideBottomNav:false,
+		showElementTitle:false,
+		verticalAdjustment:50,
+		showPreviewThumbs:false,
+		//easing:'easeOutBounce',
+		numberOfVisibleItems:3,
+		nextPrevMarginTop:23,
+		playMovieMarginTop:0,
+		bottomNavMarginBottom:-10
+	});		
+});
